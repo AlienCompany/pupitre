4b746b93060de7cf14921df7f53a34e253fd3944
Notraly <fernandez.coralie2001@gmail.com>
2019-04-04 00:09:04 +0200

    feat(board): Add nano board config

--- cmake/CustomBoard.cmake
+++ cmake/CustomBoard.cmake
@@ -1,3 +1,7 @@
 set(mega.build.mcu atmega2560)
 set(mega.upload.protocol wiring)
 set(mega.upload.speed 115200)
+
+set(nano.build.mcu atmega328p)
+set(nano.upload.protocol arduino)
+set(nano.upload.speed 57600)
